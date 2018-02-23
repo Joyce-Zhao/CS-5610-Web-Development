5f4167cf4f01592b270b18a1191eb0c7a3e06732
Joyce <joyce.zhao.de@gmail.com>
2018-02-23 01:49:08 -0500

    part

--- homeworks/tasktracker/config/dev.exs
+++ homeworks/tasktracker/config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :tasktracker, Tasktracker.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "tasktracker",
+  password: "joyce951125",
   database: "tasktracker_dev",
   hostname: "localhost",
   pool_size: 10
